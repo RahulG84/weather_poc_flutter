f97b3945d0fdce5aabd3cb4264a67847803214ed
Rahul Girgal <rahul4girgal@gmail.com>
2024-02-19 10:46:01 +0530

    Added : Initial Folder Structure  Setup

---
+++ lib/home/ui/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Weather App'),
+      ),
+      body: Container(
+        child: const Column(
+          children: [
+            Text('Home Page'),
+          ],
+        ),
+      ),
+    );
+  }
+}
